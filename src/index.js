c18d84406fa79f61440c6a702b095a6fc5e77229
Real Inspiration <ilerwatson22@gmail.com>
2022-01-05 19:20:47 +0000

    Async function created to to list question and provide answer in object

--- src/index.js
+++ src/index.js
@@ -7,3 +7,22 @@ const questions = [
     name: "title",
   },
 ];
+
+const inquirerAsync = async (questions) => {
+  try {
+    const answers = await inquirer.prompt(questions);
+
+    return answers;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(0);
+  }
+};
+
+const start = async () => {
+  const answers = await inquirerAsync(questions);
+
+  console.log(answers);
+};
+
+start();
