7b81abd252c4d0a168fdb0e04773b60e53cf592f
Real Inspiration <ilerwatson22@gmail.com>
2022-01-07 19:29:05 +0000

    WriteFileSync updated to create readme in separate folder

--- src/file.js
+++ src/file.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const writeToFile = (data) => {
   try {
-    fs.writeFileSync('README.md', data);
+    fs.writeFileSync('./lib/README.md', data);
   } catch (error) {
     console.log(`[ERROR]: Failed to write to file | ${error.message}`);
   }
